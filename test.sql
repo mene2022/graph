ea05786bde1d8ef3c68b6d48c68900c6558fe878
mene2022 <elhadjmene@gmail.com>
2023-05-22 13:17:34 +0200

    ajout fichier seeding

--- test.sql
+++ test.sql
@@ -1,120 +1,68 @@
--- Deploy streamer:create_all_tables to pg
-
 BEGIN;
 
+-- Role table
+INSERT INTO "role" ("name") VALUES 
+('Admin'),
+('User');
 
-CREATE TABLE "role" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "name" TEXT NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
-);
+-- User table
+INSERT INTO "user" ("username", "email", "password", "age", "gender", "role_id") VALUES 
+('admin', 'admin@example.com', 'password_example', 30, 'Male', 1),
+('user1', 'user1@example.com', 'password_example', 22, 'Female', 2);
 
-CREATE TABLE "user" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "username" TEXT NOT NULL,
-  "password" TEXT NOT NULL,
-  "role_id" INTEGER NOT NULL REFERENCES role("id"),
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
-);
-CREATE TABLE "topic" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "title" TEXT NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
-);
+-- Message table
+INSERT INTO "message" ("content", "user_id") VALUES 
+('Hello world!', 1),
+('Hello everyone!', 2);
 
+-- Post table
+INSERT INTO "post" ("title", "content", "message_id") VALUES 
+('First Post', 'Welcome to our community!', 1),
+('Second Post', 'Enjoy your stay here!', 2);
 
-CREATE TABLE "post" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "title" TEXT NOT NULL,
-  "content" TEXT NOT NULL,
-  "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
-  --"topic_id" INTEGER NOT NULL REFERENCES topic("id"),
-  "message_id" INTEGER NOT NULL REFERENCES message("id")
-   "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-  "updated_at" TIMESTAMPTZ
-);
+-- Topic table
+INSERT INTO "topic" ("title", "post_id") VALUES 
+('Introduction', 1),
+('Getting started', 2);
 
-CREATE TABLE "message" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "content" TEXT NOT NULL,
-  "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ,
-  "post_id" INTEGER NOT NULL REFERENCES post("id")
-);
+-- Drawing table
+INSERT INTO "drawing" ("name", "data", "user_id") VALUES 
+('Drawing1', 'data1', 1),
+('Drawing2', 'data2', 2);
 
-CREATE TABLE "drawing" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-   "data" BYTEA NOT NULL,
-   "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-    "updated_at" TIMESTAMPTZ
-  "user_id" INTEGER NOT NULL REFERENCES "user"("id")
-);
+-- Drawing Comment table
+INSERT INTO "drawing_comment" ("content", "drawing_id") VALUES 
+('Nice drawing!', 1),
+('Great job!', 2);
 
-CREATE TABLE "drawing_comment" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "content" TEXT NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ,
-  "drawing_id" INTEGER NOT NULL REFERENCES drawing("id")
-);
+-- Video table
+INSERT INTO "video" ("url", "name", "type") VALUES 
+('https://example.com/video1', 'Video1', 'mp4'),
+('https://example.com/video2', 'Video2', 'mp4');
 
-CREATE TABLE "video" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "url" TEXT NOT NULL,
-  "type" TEXT,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
-);
+-- Video Comment table
+INSERT INTO "video_comment" ("content", "video_id") VALUES 
+('Nice video!', 1),
+('Great job!', 2);
 
-CREATE TABLE "video_comment" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "content" TEXT NOT NULL,
-  "video_id" INTEGER NOT NULL REFERENCES video("id"),
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
-);
+-- Drawing Like table
+INSERT INTO "drawing_like" ("user_id", "drawing_id") VALUES 
+(1, 2),
+(2, 1);
 
-CREATE TABLE "drawing_like" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
-  "drawing_id" INTEGER NOT NULL REFERENCES drawing("id"),
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
+-- Comment table
+INSERT INTO "comment" ("user_id", "drawing_comment_id") VALUES 
+(1, 2),
+(2, 1);
 
-);
+-- Video Like table
+INSERT INTO "video_like" ("user_id", "video_id") VALUES 
+(1, 2),
+(2, 1);
 
-CREATE TABLE "comment" (
-  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
-  "drawing_comment_id" INTEGER NOT NULL REFERENCES drawing_comment("id"),
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
-);
-
-CREATE TABLE "video_like" (
-   "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
-  "video_id" INTEGER NOT NULL REFERENCES video("id"),
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
-);
-
-CREATE TABLE "publish" (
- "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
- "video_comment_id" INTEGER NOT NULL REFERENCES comment("id"),
- "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-   "updated_at" TIMESTAMPTZ
- 
-);
-
-
-  
-
-
+-- Publish table
+INSERT INTO "publish" ("user_id", "video_comment_id") VALUES 
+(1, 2),
+(2, 1);
 
 COMMIT;
